fe038f5e065bb57d650267137e55eb2e065c187e
abid365 <abidibnazam@gmail.com>
2024-11-27 14:07:57 +0600

    updated gitignore

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require("path");
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'public'))
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render('index', { title: 'Welcome', message: 'hello world' })
+
+})
+
+io.on('connection', (socket) => {
+    console.log(`A user is connected ${socket.id}`)
+    socket.on('chat message', (msg) => {
+        console.log('message:', msg)
+        io.emit('chat message', msg);
+    })
+    socket.on('location', (loc) => {
+        console.log(loc);
+        io.emit('location', loc);
+    })
+})
+
+server.listen(3000, () => {
+    console.log(`Server is runnnng at 3000`)
+})

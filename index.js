9bee8bcfa7fb06ca7ee466f7105072dbcd869f39
abid365 <abidibnazam@gmail.com>
2024-11-27 15:33:11 +0600

    config:render host

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,
+    {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            transports: ['websocket', 'polling'],
+            credentials: true
+        },
+        allowEIO3: true
+    }
+);
 const path = require("path");
 
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'public'))
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/", (req, res) => {
     res.render('index', { title: 'Welcome', message: 'hello world' })
